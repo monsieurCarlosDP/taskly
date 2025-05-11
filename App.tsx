f56b2dc950c82a01f3fa1bd4ec6f73a4fc188a33
monsieurCarlosDP <cdperez.85@gmail.com>
2025-05-11 23:38:22 +0200

    Primeros pasos en componentes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Alert, TouchableOpacity, Pressable } from 'react-native';
+import { theme } from './theme';
 
 export default function App() {
+
+  const handleDelete = ()=>{
+    Alert.alert("Tas seguro, hermoso?", "La puedes liar...", [{
+      text: "poj caro",
+      onPress: () => console.log('sep'),
+      style: 'destructive'
+    },
+    {
+      text: "mmmeh",
+      onPress: ()=> console.log('mmmmeh'),
+      style: 'default'
+    }
+  ]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+        <TouchableOpacity onPress={handleDelete} style={styles.button} activeOpacity={0.8}>
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +35,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    
     justifyContent: 'center',
   },
+  textContainer: {
+    borderBottomWidth: 1,
+    paddingHorizontal: 8,
+    paddingVertical: 12,
+    borderBottomColor: '#ff007d',
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  text: {
+    fontSize: 12
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 6,
+    borderRadius: 6
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    letterSpacing: 1
+  }
 });
---
+++ theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+    colorMagenta: '#ff007d',
+    colorWhite: '#f7f8fb',
+    colorBlack: '#002'
+}
